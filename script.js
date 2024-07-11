6f91e265f8f5b8db8f3d8ab3b7f6041dbb64f151
harshad-22 <154778159+harshad-22@users.noreply.github.com>
2024-07-11 16:36:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the Track Now button element
+const trackButton = document.getElementById('track-button');
+
+// Add a click event listener to the button
+trackButton.addEventListener('click', function() {
+    // Redirect to the login page when the button is clicked
+    window.location.href = 'login.html'; // Replace 'login.html' with your actual login page URL
+});
